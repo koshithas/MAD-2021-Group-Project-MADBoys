46b2e99edb0e86bf6a19099dcc25fbe95d85ec02
Koshitha Amarakoon <koshithas@users.noreply.github.com>
2021-03-19 21:35:36 +0530

    Ui designs

---
+++ app/src/main/java/com/techdecode/pizzaapp/EditAndDeletePaymentData.java
@@ -0,0 +1,14 @@
+package com.techdecode.pizzaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditAndDeletePaymentData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_and_delete_payment_data);
+    }
+}
---
+++ app/src/main/java/com/techdecode/pizzaapp/InsertDelivery.java
@@ -0,0 +1,14 @@
+package com.techdecode.pizzaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertDelivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_delivery);
+    }
+}
